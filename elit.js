266ead6915317ddcd66eb5fb0aa27179de0d1467
cedric michel <cedre070@aol.com>
2016-12-06 17:04:49 +0100

    navigation

--- elit.js
+++ elit.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-
-
-
-
    var scroll_start = 0;
    var startchange = $('header');
    var offset = startchange.offset();
+
    $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('header').css('background-color', '#f0f0f0');
+          $('#navigation').css('background-color', 'rgba(0, 0, 0, 0.3)');
+          $('#logo').css('width', '30%');
+          $('.navig').css('padding-top', '35px');
+          $('#logo').css('padding-bottom', '20px');
        } else {
           $('#navigation').css('background-color', 'transparent');
+          $('#logo').css('width', '160px');
        }
    });
- });
+});
